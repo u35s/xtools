8b300766b4c1841a827fab2546dd3087e97346a7
u35s <jintao7080@gmail.com>
2018-11-25 15:39:58 +0800

    add libssh2

---
+++ cmd/xsh.cc
@@ -0,0 +1,34 @@
+/*
+ * Copyright [2018] <Copyright u35s>
+ */
+
+#include "xlib/log.h"
+
+#include "common/options.h"
+#include "common/config.h"
+
+int main(int argc, char **argv) {
+    common::Options options;
+    options.Init(argc, argv);
+
+    if (options.log_file.empty() == false) {
+        xlib::Log::Instance().SetLogFile(options.log_file);
+    }
+    if (options.log_level != 0) {
+        xlib::Log::Instance().SetLogPriority(xlib::LOG_PRIORITY(options.log_level));
+    }
+    XINF("xsh 1.0")
+
+    common::Config config;
+    config.Init("../conf/example.conf");
+    common::Host host;
+    int index = 1;
+    config.GetHost("a", index, &host);
+    XINF("get a-%v, %v ,tag num %v", index, host.alias, host.tags.size());
+
+    std::string tag("tag1");
+    std::vector<common::Host> hosts;
+    config.GetHostsByTag(tag, &hosts);
+    XINF("get %v, num %v", tag, hosts.size());
+    return 0;
+}
--- tools/check.sh
+++ tools/check.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
-find src -name "*.cc" | xargs ./tools/cpplint.py --root=src 
-find src -name "*.h"  | xargs ./tools/cpplint.py --root=src 
+find src -name "*.cc" | xargs -r ./tools/cpplint.py --root=src 
+find src -name "*.h"  | xargs -r ./tools/cpplint.py --root=src 
+find cmd -name "*.cc" | xargs -r ./tools/cpplint.py --root=cmd 
+find cmd -name "*.h"  | xargs -r ./tools/cpplint.py --root=cmd 
