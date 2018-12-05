9280c5a46933e2bb25fb9119a27ba4d0a4ab701e
u35s <jintao7080@gmail.com>
2018-12-05 17:55:41 +0800

    注释修改

--- src/common/ssh2_client.cc
+++ src/common/ssh2_client.cc
@@ -397,7 +397,7 @@ void SSH2Client::Login() {
         }
 
         if (fds[0].revents & LIBSSH2_POLLFD_CHANNEL_CLOSED) {
-            //fprintf(stdout, "%s\n", "closed");
+            // fprintf(stdout, "%s\n", "closed");
             break;
         }
     }
